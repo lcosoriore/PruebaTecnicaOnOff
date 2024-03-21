87a3387673d09fe2c109060f6eaf0cb6646fe275
Luisa Carolina Osorio Restrepo <luisa.osoriore@gmail.com>
2024-03-21 09:01:24 -0500

    01
    Commit inicial creacion de proyecto

---
+++ Sorteo.Infraestructura/Middleware/ApiKeyMiddleware.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Sorteo.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorteo.Infrastructure.Middleware
+{
+    public class ApiKeyMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly IApiKeyRepository _apiKeyRepository;
+
+        public ApiKeyMiddleware(RequestDelegate next, IApiKeyRepository apiKeyRepository)
+        {
+            _next = next;
+            _apiKeyRepository = apiKeyRepository;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            if (!context.Request.Headers.TryGetValue("ApiKey", out var apiKeyHeader))
+            {
+                context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+                await context.Response.WriteAsync("ApiKey missing.");
+                return;
+            }
+
+            var apiKey = apiKeyHeader.FirstOrDefault();
+            if (!_apiKeyRepository.IsValidApiKey(apiKey))
+            {
+                context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+                await context.Response.WriteAsync("Invalid ApiKey.");
+                return;
+            }
+
+            await _next.Invoke(context);
+        }
+    }
+
+}
